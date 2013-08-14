01a77a366423b136c715577c72cdd11879e98d5c
William Hart <goodwillhart@googlemail.com>
2013-08-14 04:27:47 +0100

    Removed large comment section.

--- red.c
+++ red.c
@@ -7,37 +7,6 @@ int main(void)
    GC_INIT();
    init();
 
-   /*_var("i", _int(0))()();
-   _var("s", _int(0))()();
-   
-   del_t arr[] = { _inc(_ident("i")), _addeq(_ident("s"), _ident("i")) };
-
-   _while(_lt(_ident("i"), _int(10)), _block(arr))()();
-
-   _println(_ident("s"))()();
-
-   const char * params[] = { "a" };
-
-   del_t body[] = { _var("k", _ident("a")), _inc(_ident("k")), _return(_ident("k")) };
-
-   _fn("bill", params, body)()();
-
-   del_t vals[] = { _int(3) };
-
-   _println(_call("bill", vals))()();
-
-   const char * params2[] = { "n" };
-
-   del_t vals3[] = { _sub(_ident("n"), _int(1)) };
-
-   del_t body2[] = { _if_else(_le(_ident("n"), _int(1)), _return(_int(1)), _return(_mul(_ident("n"), _call("fac", vals3)))) };
-
-   _fn("fac", params2, body2)()();
-
-   del_t vals2[] = { _int(20) };
-
-   _println(_call("fac", vals2))()();*/
-
    int jval = 0;
    del_t root;
    
